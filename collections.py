ffc0b03dd4dc02d4bf2ca4167992acf05654780e
Grant Lemons <grantlemons@users.noreply.github.com>
2021-07-05 23:58:45 -0500

    Creating variables

---
+++ collections.py
@@ -0,0 +1,39 @@
+# Lists
+# -----------------------------
+# Lists are ordered, changeable, and allow duplicate values
+# Lists are created with square brackets
+
+my_list = ['lists', 'tuples', 'sets', 'dictionaries']
+
+# Tuples
+# -----------------------------
+# Tuples are ordered, unchangeable, and allow duplicate values
+# Once a tuple is created no values can be changed, removed, or added
+# Tuples are created with round brackets
+
+my_tuple = ('lists', 'tuples', 'sets', 'dictionaries')
+
+# Sets
+# -----------------------------
+# Sets are unordered, unindexed, and don't allow duplicate values
+# One cannot reference something by index as they don't have and order
+# Sets are created with curly brackets
+
+my_set = {'no', 'one', 'uses', 'these'}
+
+# Dictionaries
+# -----------------------------
+# Dictionaries are ordered, changeable, and don't allow duplicates
+# Dictionaries use key:value pairs
+# Dictionaries are created with curly brackets
+
+my_dictionary = {
+    'mitochondria': 'powerhouse of the cell',
+    'quarks': 'really small',
+    'data types': {
+        'list': 'ordered, changeable, and allow duplicate values',
+        'tuple': 'ordered, unchangeable, and allow duplicate values',
+        'set': 'unordered, unindexed, and don\'t allow duplicate values',
+        'dictionary': 'ordered, changeable, and don\'t allow duplicates'
+    }
+}
